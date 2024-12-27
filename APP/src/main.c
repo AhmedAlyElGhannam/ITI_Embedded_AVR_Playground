a646b4078bc52abba65fd04f4ddfef3f18981e4e
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-12-28 01:32:59 +0200

    MPORT driver is fully finished

--- APP/src/main.c
+++ APP/src/main.c
@@ -11,23 +11,16 @@ int main(void)
 	/* testing MPORT functions */
 
 	// setting direction
-
-	MPORT_enuSetPinDirection(MPORT_PIN_A1, MPORT_PORT_PIN_OUTPUT); // led
-
-
-
-
-	// defining variable for button state
-
+	MPORT_voidInit();
 
 	while (true)
 	{
 		// reading button 1 state (PA0)
 
 
-			MDIO_enuSetPinValue(MDIO_PORTA, MDIO_PIN1, MDIO_PIN_HIGH); 
+			MDIO_enuSetPinValue(MDIO_PORTA, MDIO_PIN0, MDIO_PIN_HIGH); 
 		_delay_ms(500);
-			MDIO_enuSetPinValue(MDIO_PORTA, MDIO_PIN1, MDIO_PIN_LOW); 
+			MDIO_enuSetPinValue(MDIO_PORTA, MDIO_PIN0, MDIO_PIN_LOW); 
 
 		_delay_ms(500);
 	}
